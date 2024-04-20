1b06266eaf2351cf1806ac43beed5fde4311f0a1
Thulani Malaza <thmalaz022@student.wethinkcode.co.za>
2024-04-20 03:13:57 +0200

    added stories section

---
+++ javascript/index.js
@@ -0,0 +1,3 @@
+const users = ["Mavela Xakwa", "Sithembiso Buthelezi", "Anga viola Xakwa",
+            "Agcobile Mgcozi Dynani", "Mxolicy Mpondombi","Goodman Vincent",
+            "Mcebo Magagula","Thulani Thyaphile","Annelah Ndlovu"]
