3eb624bb8b5dfa156911a10f2ad8a0d774d655cd
thulani malaza <tmalaza429@gmail.com>
2024-04-23 10:31:09 +0200

    added second nav

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,3 @@
-const users = ["Mavela Xakwa", "Sithembiso Buthelezi", "Anga viola Xakwa",
+const users = ["Mavela Hezekiel Xakwa", "Sithembiso Buthelezi", "Anga viola Xakwa",
             "Agcobile Mgcozi Dynani", "Mxolicy Mpondombi","Goodman Vincent",
             "Mcebo Magagula","Thulani Thyaphile","Annelah Ndlovu"]
